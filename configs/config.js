56d09e571868dbb8851954e19ebb5a818f8bdfe0
Smana <smainklh@gmail.com>
2016-04-17 16:48:05 +0000

    First OK version

--- configs/config.js
+++ configs/config.js
@@ -5,18 +5,14 @@ var config;
 config = {
   production: {
     url: 'http://ghost.example.com',
-    forceAdminSSL: true,
     database: {
       client: 'mysql',
       connection: {
-        host: '{{ REPLACE WITH THE MYSQL SERVICE IP }}',
+        host: '{{ MYSQL K8S SERVICE IP }}',
         user: 'ghost',
         password: 'myp4ss',
         database: 'ghost',
         charset: 'utf8',
-        ssl: {
-          ca: fs.readFileSync('/etc/ghost/ssl/database-ca.crt'),
-        }
       }
     },
     server: {
